6bb30ec825ac02b567bacbdb864bec72c6c8cfac
Ernesta Unikaite <ernesta.unikaite@gmail.com>
2023-03-05 23:24:10 +0200

    update

---
+++ New folder/HelloWorld.py
@@ -0,0 +1,11 @@
+print("Hello world")
+
+a=1
+b=2
+v=a+b
+print(v)
+
+x=-5
+y=-5
+sum=x+y
+print(sum)
---
+++ practice.py
@@ -0,0 +1,4 @@
+name = "Ernesta"
+surname = "Un"
+fullname=name+" "+surname
+print(f"Hello {fullname}")
