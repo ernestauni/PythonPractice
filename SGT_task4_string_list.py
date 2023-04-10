ce3a719f3b5aa39699fdde17f6309c604097380a
Ernesta Unikaite <ernesta.unikaite@gmail.com>
2023-04-10 04:55:59 +0300

    Update

---
+++ SGT_task4_string_list.py
@@ -0,0 +1,9 @@
+# 1)Ask the user to enter the text and a letter. Count how many occurrences of the letter provided. 
+
+text=input('Enter the word: \n')
+letter=input('Enter the letter: \n')
+
+print(text.count(letter))
+
+# 1.1) Based on the task 1, count the occurrences of each character in the text provided and display them in the output
+#2)Write the program to sort the list (without using sort function). You can implement any algorithm
